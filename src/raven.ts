cd8f0f37a1c47f6244b7c8f773f1cf24ab6f299f
Christian Dupuis <cd@atomist.com>
2018-05-18 17:06:53 +0200

    revert previous change

--- src/raven.ts
+++ src/raven.ts
@@ -28,7 +28,7 @@ export class RavenAutomationEventListener extends AutomationEventListenerSupport
 
     private reportError(type: string, ctx: HandlerContext, error: any) {
         const nsp = (ctx as any as AutomationContextAware).context;
-        if (error && error instanceof Error) {
+        if (error) {
             this.raven.captureException(
                 error,
                 {
